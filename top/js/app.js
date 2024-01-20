a54a822d9da55800ce3dd276891c636d90506787
tonsok <ryutalejan@gmail.com>
2024-01-20 12:35:08 +0900

    width1440px時のレイアウト崩れ修正,cssアニメーション追加

--- top/js/app.js
+++ top/js/app.js
@@ -20,59 +20,63 @@ const swiper2 = new Swiper(".bridelfair-slides .swiper", {
   },
 });
 
+const filterButtons = document.querySelectorAll("[data-filter]");
 
-const filterButtons = document.querySelectorAll('[data-filter]');
-
-
-const categoryContents = document.querySelectorAll('[data-category]');
-
+const categoryContents = document.querySelectorAll("[data-category]");
 
 filterButtons.forEach((filterButton) => {
-  filterButton.addEventListener('click', buttonSwitch);
-  filterButton.addEventListener('click', categoryFilter);
+  filterButton.addEventListener("click", buttonSwitch);
+  filterButton.addEventListener("click", categoryFilter);
 });
 
-
 function buttonSwitch() {
-  
   filterButtons.forEach((filterButton) => {
     filterButton.classList.remove("is-active");
-    this.classList.add('is-active');
+    this.classList.add("is-active");
   });
 }
 
-
 function categoryFilter() {
-  
   const buttonCategory = this.dataset.filter;
-  
-  const targetContents = document.querySelectorAll('[data-category="' + buttonCategory + '"]');
 
-  
+  const targetContents = document.querySelectorAll(
+    '[data-category="' + buttonCategory + '"]'
+  );
+
   categoryContents.forEach((categoryContent) => {
+    categoryContent.animate([{ opacity: 0 }, { opacity: 1 }], {
+      duration: 600,
+      fill: "forwards",
+    });
 
-    
-    categoryContent.animate([
-      { opacity: 0 },
-      { opacity: 1 }
-    ],
-      { duration: 600, fill: 'forwards' }
-    );
-
-    
-    if (buttonCategory == 'all') {
-      categoryContent.classList.add('is-show');
-    }
-
-   
-    else {
-
+    if (buttonCategory == "all") {
+      categoryContent.classList.add("is-show");
+    } else {
       categoryContent.classList.remove("is-show");
 
       targetContents.forEach((targetContent) => {
-        targetContent.classList.add('is-show');
+        targetContent.classList.add("is-show");
       });
     }
   });
 }
 
+const headings = document.querySelectorAll(".contents-heading");
+
+const options = {
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver(showElements);
+
+headings.forEach((heading) => {
+  observer.observe(heading);
+});
+
+function showElements(entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+}
